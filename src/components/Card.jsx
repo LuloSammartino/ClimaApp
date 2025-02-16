f37fb34601a51e09a586363b7cc6cb3fe465b179
Lulo <lucianosammar@gmail.com>
2025-02-16 01:48:25 -0300

    Switch imagenes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
 import axios from "axios"
 import { useState, useEffect} from "react"
 import './card.css'
+import Rainy from "../assets/rain.jpg";
+import PartlyCloudy from "../assets/cloudy_sun.jpg"
+import Cloudy from "../assets/cloudy.jpg"
+import Sunny from "../assets/sunny.jpg"
 
 
 function Card({ ciudades })  {
@@ -18,8 +22,25 @@ function Card({ ciudades })  {
     await axios.get(`http://api.weatherunlocked.com/api/current/${grados}?app_id=d2d8fdb9&app_key=fdd8dcfd0060394832bca1dda09ed035`)
     .then(res => {setClima(res.data.wx_desc);
       setCelsius(res.data.temp_c);
+      console.log(res.data)
       setCiudad(found.name);
-      
+    
+      switch (res.data.wx_desc){
+        case "Partly cloudy" : setImage(PartlyCloudy)
+        
+        break;
+        case "Cloudy":
+        case "Mostly cloudy" : setImage(Cloudy);
+
+        break;
+        case "Rain Shower": setImage(Rainy);
+
+        break;
+        case "Sunny skies":
+        case "Clear skies" : setImage(Sunny);
+        
+        break;
+      }
     })
     
   }
@@ -31,7 +52,7 @@ function Card({ ciudades })  {
           {ciudades && ciudades.map(ciudad => <option>{ciudad.name}</option>)}
         </select>
         <h3>📍{ciudad}</h3>
-        <img src='../assets/rain.jpg'  width='100px' height='100px' alt="imagen"></img>
+        <img src={image} width={"100px"} alt="imagen"/>
         <h3>{celcius}°</h3>
         <h4>{clima}</h4>
         
